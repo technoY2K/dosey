0bddba5f33f5f35bed271414baf9b884ba91eb42
technoY2K <cloud@kevia.me>
2022-03-01 16:48:41 -0700

    chore: got a yesod hello world running

---
+++ app/HelloWorld.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes       #-}
+{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE TypeFamilies      #-}
+
+import           Yesod
+
+data HelloWorld = HelloWorld
+
+mkYesod "HelloWorld" [parseRoutes|/ HomeR GET|]
+
+instance Yesod HelloWorld
+
+getHomeR :: Handler Html
+getHomeR = defaultLayout [whamlet|HelloWorld!|]
+
+main :: IO ()
+main = warp 3000 HelloWorld
